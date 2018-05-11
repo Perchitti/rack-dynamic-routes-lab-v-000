edce7c9215aae2b6d5595e9a1573ebd2c468fa4d
Lauren Perchitti <laurenperchitti@gmail.com>
2018-05-11 18:12:57 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
       item = @@items.find {|i| i.name == item_name}
       if !item.nil?
         resp.write item.price
-      resp.write "You requested item: #{price}"
+        resp.status = 200
     else
       resp.write "Route not found"
       resp.status = 404
